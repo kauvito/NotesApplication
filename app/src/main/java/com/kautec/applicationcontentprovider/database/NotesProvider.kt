6250d6d2b03a19a29d361633af3d53f11c8d070f
Kauan <kauan1989@gmail.com>
2021-03-25 22:58:55 -0300

    Criação da DBHelper e ContentProvider

---
+++ app/src/main/java/com/kautec/applicationcontentprovider/database/NotesProvider.kt
@@ -0,0 +1,108 @@
+package com.kautec.applicationcontentprovider.database
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.Context
+import android.content.UriMatcher
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.media.UnsupportedSchemeException
+import android.net.Uri
+import android.provider.BaseColumns._ID
+import com.kautec.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TABLE_NOTES
+
+class NotesProvider : ContentProvider() {
+
+    private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDatabaseHelper
+
+    override fun onCreate(): Boolean {
+        mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
+        mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
+        mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+
+        if (context != null){ dbHelper = NotesDatabaseHelper(context as Context)}
+
+        return true
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        if (mUriMatcher.match(uri) == NOTES_BY_ID) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val lineAffect = db.delete(TABLE_NOTES, "$_ID =?", arrayOf(uri.lastPathSegment))
+            db.close()
+
+            context?.contentResolver?.notifyChange(uri, null)
+            return lineAffect
+        } else {
+            throw UnsupportedSchemeException("Uri invalida para exclusão")
+        }
+    }
+
+    //Usado para requisição de arquivos
+    override fun getType(uri: Uri): String? { throw UnsupportedSchemeException("Não implementado")}
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        if (mUriMatcher.match(uri) == NOTES) {
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id = db.insert(TABLE_NOTES, null, values)
+            val insertUri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertUri
+        }
+        else {
+            throw UnsupportedSchemeException("Uri invalida para inserção")
+        }
+    }
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        return when {
+            mUriMatcher.match(uri) == NOTES -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor = db.query(TABLE_NOTES, projection, selection, selectionArgs, null, null, sortOrder)
+                cursor.setNotificationUri(context?.contentResolver, uri)
+                cursor
+            }
+            mUriMatcher.match(uri) == NOTES_BY_ID -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor = db.query(TABLE_NOTES, projection, "$_ID = ?", arrayOf(uri.lastPathSegment), null,null, sortOrder)
+                cursor.setNotificationUri((context as Context).contentResolver, uri)
+                cursor
+            }
+            else -> {
+                throw UnsupportedSchemeException("Uri não implementada")
+            }
+        }
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        if (mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.update(TABLE_NOTES, values, "$_ID = ?", arrayOf(uri.lastPathSegment))
+            db.close()
+
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        }
+        else{
+            throw UnsupportedSchemeException("Uri não implementada no update")
+        }
+    }
+
+    companion object   {
+        const val AUTHORITY = "com.kautec.applicationcontentprovider.provider"
+        val BASE_URI = Uri.parse("content://$AUTHORITY")
+        val URI_NOTES = Uri.withAppendedPath(BASE_URI, "notes")
+        const val NOTES = 1
+        const val  NOTES_BY_ID = 2
+    }
+}
